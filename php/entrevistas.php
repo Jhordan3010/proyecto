024451de4c4b9664d1242142f10cc2d6fda6d64a
Marcheloo-7 <marcelochiriboga246@gmail.com>
2024-01-24 00:44:48 -0500

    Update entrevistas.php

--- php/entrevistas.php
+++ php/entrevistas.php
@@ -16,7 +16,7 @@
         <section class="botones-menu">
             <a href="../php/entrevista-gerente-general.php"><button><b>Evaluación Gerente General</b></button></a>
             <a href="../php/entrevista-gerenteV.php"><button><b>Evaluación Gerente de Ventas</b></button></a>
-            <a href="../php/entrevista-mecanico.php"><button><b>Evaluación Mecanico</b></button></a>
+            <a href="../php/entrevista-mecanico.php"><button><b>Evaluación Mecánico</b></button></a>
             <a href="../php/entrevista-recepcionista.php"><button ><b>Evaluación Recepcionista</b></button></a>
             <a href="../html/menu.html"><button><b>Regresar</b></button></a>
         </section>
