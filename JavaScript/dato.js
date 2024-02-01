91d8eb2c075a061b66ed9f8a864515524cc1a36b
Jhordan3010 <jmhueran@utn.edu.ec>
2024-01-31 21:06:50 -0500

    ..

--- JavaScript/dato.js
+++
@@ -1,5 +0,0 @@
-function enviarValor() {
-  var valor = document.getElementById('username').value;
-  localStorage.setItem('valorCompartido', valor);
-  window.location.href = 'perfil-postulante.php';
-}
--- JavaScript/enviar-pos.js
+++
@@ -1,5 +0,0 @@
- // Obtener el valor almacenado en localStorage
- var valorRecibido = localStorage.getItem('valorCompartido');
-
- // Mostrar el valor en el segundo input
- document.getElementById('username').value = valorRecibido;
